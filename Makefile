8253a97da911613d4b5355b85cfee673634b097d
Dustin Lang <dstndstn@gmail.com>
2014-12-12 17:28:10 -0500

    show a np array

--- Makefile
+++ Makefile
@@ -1,3 +1,23 @@
 app:
 	pyinstaller --windowed --onefile hello.py
+.PHONY: app
 
+#
+# The minimal plist provided by PyInstaller designates the icon file
+# for the app as the icon-windowed.icns file in Resources. This is the
+# PyInstaller logo in icns format. Support for the --icon-file option
+# is promised for the future. For now you can apply your own icon
+# after the app is built in several ways:
+#
+#    Prepare another .icns file with your own graphic, save it as
+#    icon-windowed.icns replacing the default one in Resources.
+#
+#    Prepare an .icns file with your own graphic, place it in
+#    Resources and edit the Info.plist to name it.
+#
+#    Prepare an .icns file with your own graphic; open in it
+#    Preview.app; select-all and copy; in the Finder, Get Info on your
+#    app; click the icon in the info display and paste.
+#
+# GraphicConverter is one of several applications that can save a JPEG
+# or PNG image in the .icns format.
--- hello.py
+++ hello.py
@@ -4,6 +4,8 @@ import sys
 from PySide.QtCore import *
 from PySide.QtGui import *
 
+import numpy as np
+
 '''
 
 widget.setMinimumSize(QSize(800, 600))
@@ -27,39 +29,104 @@ sys.exit(app.exec_())
 
 '''
 
-class HelloWorldApp(QLabel):
+class CustomWidget(QWidget):
+    def __init__(self):
+        super(CustomWidget, self).__init__()
+        self.qimage = None
+        
+    def setImage(self, image, format=QImage.Format_ARGB32):
+        ''' numpy image '''
+        H,W,planes = image.shape
+        #self.npimage = image.copy()
+        #print self.npimage[:4,:4,:]
+        #print 'NP image:', self.npimage
+        #self.qimage = QImage(self.npimage.data, W, H, format)
+        #self.qimage = QImage(self.npimage.tobytes(), W, H, format)
+        #self.qimage = QImage(self.npimage, W, H, format)
+
+        #a = self.npimage
+        #B = (255 << 24) | (a[:,:,1] << 16) | (a[:,:,2] << 8) | (a[:,:,3])
+        #self.qimage = QImage(B.flatten(), W, H, QImage.Format_RGB32)        
+
+        #a = self.npimage
+        #B = np.frombuffer(a.data, dtype=np.uint32, #B = (255 << 24) | (a[:,:,1] << 16) | (a[:,:,2] << 8) | (a[:,:,3])
+        self.qimage = QImage(
+            # (image[:,:,0].astype(np.uint32) << 24) | 
+            # (image[:,:,1].astype(np.uint32) << 16) | 
+            # (image[:,:,2].astype(np.uint32) << 8 ) | 
+            # (image[:,:,3].astype(np.uint32)),
+            (image[:,:,0] * (1<<24)) |
+            (image[:,:,1] * (1<<16)) |
+            (image[:,:,2] * (1<<8)) |
+            (image[:,:,3]),
+            W, H, QImage.Format_RGB32)        
+        
+        #a = np.random.randint(0,256,size=(100,100,3)).astype(np.uint32)
+        #B = (255 << 24) | (a[:,:,0] << 16) | (a[:,:,1] << 8) | (a[:,:,2])
+        #self.qimage = QImage(B.flatten(), W, H, QImage.Format_RGB32)        
+
+        #print 'bytes:', self.npimage.tobytes()
+        #print 'bytes:', repr(self.npimage.tobytes())
+        
+    def paintEvent(self, event):
+        p = QPainter(self)
+        print 'size', self.size()
+        if self.qimage is not None:
+            print 'Drawing image'
+            #p.drawImage(0, 0, self.qimage)
+            rect = QRect(QPoint(0,0), self.size())
+            print 'rect:', rect
+            print 'image size:', self.qimage.size()
+            sourcerect = QRect(QPoint(0,0), self.qimage.size())
+            #p.drawImage(rect, self.qimage, sourcerect)
+            p.drawImage(rect, self.qimage)
+            
+#class HelloWorldApp(QLabel):
+class HelloWorldApp(QWidget):
     ''' A Qt application that displays the text, "Hello, world!" '''
     def __init__(self):
+        super(HelloWorldApp, self).__init__()
         # Initialize the object as a QLabel
-        QLabel.__init__(self, "Hello, world!")
- 
+        #win = QWidget()
+        label = QLabel('Hello, world!')
+        label.setAlignment(Qt.AlignCenter)
+
+        # A vertical box layout
+        layout = QVBoxLayout()
+        layout.addWidget(label)
+
+        imagebox = CustomWidget()
+        W,H = 5,5
+        img = np.zeros((H,W, 4), np.uint8)
+        img[:,:,0] = 255
+        img[:,:,1] = np.linspace(0, 255, H).astype(np.uint8)[:,np.newaxis]
+        img[:,:,2] = np.linspace(0, 255, W).astype(np.uint8)[np.newaxis,:]
+        imagebox.setImage(img)
+        imagebox.setMinimumSize(QSize(400, 400))
+        layout.addWidget(imagebox)
+        
+        self.setLayout(layout)
+        
         # Set the size, alignment, and title
         self.setMinimumSize(QSize(600, 400))
-        self.setAlignment(Qt.AlignCenter)
         self.setWindowTitle('Hello, world!')
  
     def run(self, qt_app):
         ''' Show the application window and start the main event loop '''
         #self.showFullScreen()
         self.show()
-        #self.activateWindow()
         self.raise_()
-        #print dir(self)
-        print 'pre .exec()'
         qt_app.exec_()
-        print 'post .exec()'
 
-    '''
     def keyPressEvent(self, event):
-        QtGui.QLabel.keyPressEvent(self, event)
-        if event.key() == QtCore.Qt.Key_Escape:
-            if self.isFullScreen():
-                self.showNormal()
-                self.showMaximized()
-            else:
-                self.showFullScreen()
-    '''
-
+        super(HelloWorldApp, self).keyPressEvent(event)
+        if event.key() == Qt.Key_Escape:
+            # if self.isFullScreen():
+            #     self.showNormal()
+            #     self.showMaximized()
+            # else:
+            #     self.showFullScreen()
+            self.close()
         
 # Create the QApplication object
 qt_app = QApplication(sys.argv)
